ba3e8ed4149cc3988b2109c1f6d57dfe663ddb14
Pritee Gupta <priteegupta517@gmail.com>
2025-07-23 12:03:46 +0530

    fixed the ui of Hospital Availability screen

--- App.jsx
+++ App.jsx
@@ -1,67 +1,3 @@
-// import React, { useRef } from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { ThemeProvider } from "./contexts/ThemeContext";
-// import { ChatbotProvider } from "./contexts/ChatbotContext";
-// import ChatBotOverlay from "./components/PatientScreenComponents/ChatbotComponents/ChatbotOverlay";
-// import { AuthProvider } from "./contexts/AuthContext";
-// import { RoleProvider } from "./contexts/RoleContext";
-// import RootNavigation, { linking } from "./navigation/RootNavigator";
-
-// const App = () => {
-// 	const navigationRef = useRef(null);
-
-// 	return (
-// 		<AuthProvider>
-// 			<ThemeProvider>
-// 				<ChatbotProvider>
-// 					<RoleProvider>
-// 						<NavigationContainer
-// 							linking={linking}
-// 							ref={navigationRef}>
-// 							<RootNavigation />
-// 							<ChatBotOverlay navigationRef={navigationRef} />
-// 						</NavigationContainer>
-// 					</RoleProvider>
-// 				</ChatbotProvider>
-// 			</ThemeProvider>
-// 		</AuthProvider>
-// 	);
-// };
-
-// export default App;
-
-// import React, { useRef } from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { ThemeProvider } from "./contexts/ThemeContext";
-// import { ChatbotProvider } from "./contexts/ChatbotContext";
-// import ChatBotOverlay from "./components/PatientScreenComponents/ChatbotComponents/ChatbotOverlay";
-// import { AuthProvider } from "./contexts/AuthContext";
-// import { RoleProvider } from "./contexts/RoleContext";
-// import RootNavigation, { linking } from "./navigation/RootNavigator";
-
-// const App = () => {
-// 	const navigationRef = useRef(null);
-
-// 	return (
-// 		<AuthProvider>
-// 			<ThemeProvider>
-// 				<ChatbotProvider>
-// 					<RoleProvider>
-// 						<NavigationContainer
-// 							linking={linking}
-// 							ref={navigationRef}>
-// 							<RootNavigation />
-// 							<ChatBotOverlay navigationRef={navigationRef} />
-// 						</NavigationContainer>
-// 					</RoleProvider>
-// 				</ChatbotProvider>
-// 			</ThemeProvider>
-// 		</AuthProvider>
-// 	);
-// };
-
-// export default App;
-
 import React, { useRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -70,13 +6,7 @@ import ChatBotOverlay from "./components/PatientScreenComponents/ChatbotComponen
 import { AuthProvider } from "./contexts/AuthContext";
 import { RoleProvider } from "./contexts/RoleContext";
 import RootNavigation, { linking } from "./navigation/RootNavigator";
-import AllHospitals from "./screens/PatientScreens/Hospitals/AllHospitals";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HospitalsInfoWithRating from "./screens/PatientScreens/Hospitals/HospitalsInfoWithRating";
-import DoctorsSignUp from "./screens/DoctorScreens/DoctorRegistration/DoctorsSignUp";
-import DoctorAvailabilitySlots from "./screens/PatientScreens/Doctors/App/DoctorAvailabilitySlots";
-import HospitalAvailability from "./screens/PatientScreens/Hospitals/App/HospitalAvailability";
-const Stack = createNativeStackNavigator();
+
 const App = () => {
 	const navigationRef = useRef(null);
 
@@ -88,40 +18,13 @@ const App = () => {
 						<NavigationContainer
 							linking={linking}
 							ref={navigationRef}>
-							{/* <Stack.Navigator initialRouteName="HospitalsInfoWithRating">
-								<Stack.Screen
-									name="HospitalsInfoWithRating"
-									component={HospitalsInfoWithRating}
-								/>
-							</Stack.Navigator> */}
-							<Stack.Navigator initialRouteName="HospitalAvailability">
-								<Stack.Screen
-									name="HospitalAvailability"
-									component={HospitalAvailability}
-								/>
-							</Stack.Navigator>
+							<RootNavigation />
 							<ChatBotOverlay navigationRef={navigationRef} />
 						</NavigationContainer>
 					</RoleProvider>
 				</ChatbotProvider>
 			</ThemeProvider>
 		</AuthProvider>
-
-		// <AuthProvider>
-		// 	<ThemeProvider>
-		// 		<ChatbotProvider>
-		// 			<RoleProvider>
-		// 				<NavigationContainer
-		// 					linking={linking}
-		// 					ref={navigationRef}>
-		// 					{/* <HospitalsInfoWithRating /> */}
-		// 					<RootNavigation />
-		// 					<ChatBotOverlay navigationRef={navigationRef} />
-		// 				</NavigationContainer>
-		// 			</RoleProvider>
-		// 		</ChatbotProvider>
-		// 	</ThemeProvider>
-		// </AuthProvider>
 	);
 };
 
