657ebc0c14ae18de8263a8784692662911ae4d07
Pritee Gupta <priteegupta517@gmail.com>
2025-07-23 11:54:13 +0530

    fixed the ui of Hospital availability screen

--- App.jsx
+++ App.jsx
@@ -1,3 +1,67 @@
+// import React, { useRef } from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { ThemeProvider } from "./contexts/ThemeContext";
+// import { ChatbotProvider } from "./contexts/ChatbotContext";
+// import ChatBotOverlay from "./components/PatientScreenComponents/ChatbotComponents/ChatbotOverlay";
+// import { AuthProvider } from "./contexts/AuthContext";
+// import { RoleProvider } from "./contexts/RoleContext";
+// import RootNavigation, { linking } from "./navigation/RootNavigator";
+
+// const App = () => {
+// 	const navigationRef = useRef(null);
+
+// 	return (
+// 		<AuthProvider>
+// 			<ThemeProvider>
+// 				<ChatbotProvider>
+// 					<RoleProvider>
+// 						<NavigationContainer
+// 							linking={linking}
+// 							ref={navigationRef}>
+// 							<RootNavigation />
+// 							<ChatBotOverlay navigationRef={navigationRef} />
+// 						</NavigationContainer>
+// 					</RoleProvider>
+// 				</ChatbotProvider>
+// 			</ThemeProvider>
+// 		</AuthProvider>
+// 	);
+// };
+
+// export default App;
+
+// import React, { useRef } from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { ThemeProvider } from "./contexts/ThemeContext";
+// import { ChatbotProvider } from "./contexts/ChatbotContext";
+// import ChatBotOverlay from "./components/PatientScreenComponents/ChatbotComponents/ChatbotOverlay";
+// import { AuthProvider } from "./contexts/AuthContext";
+// import { RoleProvider } from "./contexts/RoleContext";
+// import RootNavigation, { linking } from "./navigation/RootNavigator";
+
+// const App = () => {
+// 	const navigationRef = useRef(null);
+
+// 	return (
+// 		<AuthProvider>
+// 			<ThemeProvider>
+// 				<ChatbotProvider>
+// 					<RoleProvider>
+// 						<NavigationContainer
+// 							linking={linking}
+// 							ref={navigationRef}>
+// 							<RootNavigation />
+// 							<ChatBotOverlay navigationRef={navigationRef} />
+// 						</NavigationContainer>
+// 					</RoleProvider>
+// 				</ChatbotProvider>
+// 			</ThemeProvider>
+// 		</AuthProvider>
+// 	);
+// };
+
+// export default App;
+
 import React, { useRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -6,7 +70,13 @@ import ChatBotOverlay from "./components/PatientScreenComponents/ChatbotComponen
 import { AuthProvider } from "./contexts/AuthContext";
 import { RoleProvider } from "./contexts/RoleContext";
 import RootNavigation, { linking } from "./navigation/RootNavigator";
-
+import AllHospitals from "./screens/PatientScreens/Hospitals/AllHospitals";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HospitalsInfoWithRating from "./screens/PatientScreens/Hospitals/HospitalsInfoWithRating";
+import DoctorsSignUp from "./screens/DoctorScreens/DoctorRegistration/DoctorsSignUp";
+import DoctorAvailabilitySlots from "./screens/PatientScreens/Doctors/App/DoctorAvailabilitySlots";
+import HospitalAvailability from "./screens/PatientScreens/Hospitals/App/HospitalAvailability";
+const Stack = createNativeStackNavigator();
 const App = () => {
 	const navigationRef = useRef(null);
 
@@ -18,13 +88,40 @@ const App = () => {
 						<NavigationContainer
 							linking={linking}
 							ref={navigationRef}>
-							<RootNavigation />
+							{/* <Stack.Navigator initialRouteName="HospitalsInfoWithRating">
+								<Stack.Screen
+									name="HospitalsInfoWithRating"
+									component={HospitalsInfoWithRating}
+								/>
+							</Stack.Navigator> */}
+							<Stack.Navigator initialRouteName="HospitalAvailability">
+								<Stack.Screen
+									name="HospitalAvailability"
+									component={HospitalAvailability}
+								/>
+							</Stack.Navigator>
 							<ChatBotOverlay navigationRef={navigationRef} />
 						</NavigationContainer>
 					</RoleProvider>
 				</ChatbotProvider>
 			</ThemeProvider>
 		</AuthProvider>
+
+		// <AuthProvider>
+		// 	<ThemeProvider>
+		// 		<ChatbotProvider>
+		// 			<RoleProvider>
+		// 				<NavigationContainer
+		// 					linking={linking}
+		// 					ref={navigationRef}>
+		// 					{/* <HospitalsInfoWithRating /> */}
+		// 					<RootNavigation />
+		// 					<ChatBotOverlay navigationRef={navigationRef} />
+		// 				</NavigationContainer>
+		// 			</RoleProvider>
+		// 		</ChatbotProvider>
+		// 	</ThemeProvider>
+		// </AuthProvider>
 	);
 };
 
--- components/PatientScreenComponents/Header.jsx
+++ components/PatientScreenComponents/Header.jsx
@@ -21,10 +21,10 @@ import NewestSidebar from "../DoctorsPortalComponents/NewestSidebar";
 const { width, height } = Dimensions.get("window");
 
 const Header = ({ navigation, isDoctorPortal = false }) => {
-  const { user, logout, setRole } = useContext(AuthContext);
-  const { width } = useWindowDimensions();
-  const [isSideBarVisible, setIsSideBarVisible] = useState(false);
-  const [dropdownVisible, setDropdownVisible] = useState(false
+	const { user, logout, setRole } = useContext(AuthContext);
+	const { width } = useWindowDimensions();
+	const [isSideBarVisible, setIsSideBarVisible] = useState(false);
+	const [dropdownVisible, setDropdownVisible] = useState(false);
 
 	// const handleOptionPress = (screen) => {
 	//   setDropdownVisible(false);
@@ -261,349 +261,347 @@ const Header = ({ navigation, isDoctorPortal = false }) => {
 						</View>
 					)}
 
-          {(Platform.OS !== "web" || width < 1000) && (
-            <>
-              <Modal
-                visible={isSideBarVisible}
-                transparent={true}
-                onRequestClose={() => setIsSideBarVisible(false)}
-              >
-                <View style={styles.modalContainer}>
-                  <View style={styles.mobileSidebar}>
-                    {isDoctorPortal ? (
-                      <NewestSidebar 
-                        navigation={navigation}
-                        closeSidebar={() => setIsSideBarVisible(false)}
-                      />
-                    ) : (
-                      <SideBarNavigation
-                        navigation={navigation}
-                        closeSidebar={() => setIsSideBarVisible(false)}
-                      />
-                    )}
-                  </View>
-                  <Pressable
-                    style={styles.overlay}
-                    onPress={() => setIsSideBarVisible(false)}
-                  />
-                </View>
-              </Modal>
-              <View style={styles.appHeaderContainer}>
-                <View style={styles.appHeader}>
-                  <View style={styles.logo}>
-                    <Pressable
-                      style={styles.hamburger}
-                      onPress={() => setIsSideBarVisible(true)}
-                    >
-                      <MaterialIcons name="menu" size={30} color="black" />
-                    </Pressable>
-                    <Image
-                      source={require("../../assets/Images/KokoroLogo.png")}
-                      style={{ height: 30, width: 30 }}
-                    />
-                    <Text
-                      style={{
-                        fontWeight: "800",
-                        color: "#000000",
-                        fontSize: 16,
-                      }}
-                    >
-                      Kokoro.Doctor
-                    </Text>
-                  </View>
+					{(Platform.OS !== "web" || width < 1000) && (
+						<>
+							<Modal
+								visible={isSideBarVisible}
+								transparent={true}
+								onRequestClose={() => setIsSideBarVisible(false)}>
+								<View style={styles.modalContainer}>
+									<View style={styles.mobileSidebar}>
+										{isDoctorPortal ? (
+											<NewestSidebar
+												navigation={navigation}
+												closeSidebar={() => setIsSideBarVisible(false)}
+											/>
+										) : (
+											<SideBarNavigation
+												navigation={navigation}
+												closeSidebar={() => setIsSideBarVisible(false)}
+											/>
+										)}
+									</View>
+									<Pressable
+										style={styles.overlay}
+										onPress={() => setIsSideBarVisible(false)}
+									/>
+								</View>
+							</Modal>
+							<View style={styles.appHeaderContainer}>
+								<View style={styles.appHeader}>
+									<View style={styles.logo}>
+										<Pressable
+											style={styles.hamburger}
+											onPress={() => setIsSideBarVisible(true)}>
+											<MaterialIcons
+												name="menu"
+												size={30}
+												color="black"
+											/>
+										</Pressable>
+										<Image
+											source={require("../../assets/Images/KokoroLogo.png")}
+											style={{ height: 30, width: 30 }}
+										/>
+										<Text
+											style={{
+												fontWeight: "800",
+												color: "#000000",
+												fontSize: 16,
+											}}>
+											Kokoro.Doctor
+										</Text>
+									</View>
 
-                  <View style={styles.authButtonsApp}>
-                    <Pressable
-                      onPress={() => setDropdownVisible(!dropdownVisible)}
-                    >
-                      <MaterialIcons name="person" size={30} color="black" />
-                    </Pressable>
-                    <Pressable>
-                      <MaterialIcons
-                        name="notifications"
-                        size={30}
-                        color="black"
-                      />
-                    </Pressable>
-                    {dropdownVisible && (
-                      <View
-                        style={[styles.dropdownMain, styles.dropdownLoggedOut]}
-                      >
-                        <Pressable
-                          //onPress={() => handleOptionPress("Login")}
-                          onPress={() => navigation.navigate("Login")}
-                          style={styles.dropdownItem}
-                        >
-                          <Text style={styles.dropdownText}>Login</Text>
-                        </Pressable>
-                        <Pressable
-                          ///onPress={() => handleOptionPress("Signup")}
-                          onPress={handleOptionPress}
-                          style={styles.dropdownItem}
-                        >
-                          <Text style={styles.dropdownText}>Signup</Text>
-                        </Pressable>
-                      </View>
-                    )}
-                  </View>
-                </View>
-                {!isDoctorPortal && (
-                  <View style={styles.usernameApp}>
-                    <Text
-                      style={{
-                        fontWeight: "600",
-                        color: "#000000",
-                        fontSize: 20,
-                      }}
-                    >
-                      Hello,
-                    </Text>
-                    <Text
-                      style={{
-                        fontWeight: "800",
-                        color: "#000000",
-                        fontSize: 20,
-                      }}
-                    >
-                      {" "}
-                      {user?.name ? user?.name : "User"}!
-                    </Text>
-                  </View>
-                )}
-              </View>
-            </>
-          )}
-        </>
-      )}
-    </View>
-  );
+									<View style={styles.authButtonsApp}>
+										<Pressable
+											onPress={() => setDropdownVisible(!dropdownVisible)}>
+											<MaterialIcons
+												name="person"
+												size={30}
+												color="black"
+											/>
+										</Pressable>
+										<Pressable>
+											<MaterialIcons
+												name="notifications"
+												size={30}
+												color="black"
+											/>
+										</Pressable>
+										{dropdownVisible && (
+											<View
+												style={[styles.dropdownMain, styles.dropdownLoggedOut]}>
+												<Pressable
+													//onPress={() => handleOptionPress("Login")}
+													onPress={() => navigation.navigate("Login")}
+													style={styles.dropdownItem}>
+													<Text style={styles.dropdownText}>Login</Text>
+												</Pressable>
+												<Pressable
+													///onPress={() => handleOptionPress("Signup")}
+													onPress={handleOptionPress}
+													style={styles.dropdownItem}>
+													<Text style={styles.dropdownText}>Signup</Text>
+												</Pressable>
+											</View>
+										)}
+									</View>
+								</View>
+								{!isDoctorPortal && (
+									<View style={styles.usernameApp}>
+										<Text
+											style={{
+												fontWeight: "600",
+												color: "#000000",
+												fontSize: 20,
+											}}>
+											Hello,
+										</Text>
+										<Text
+											style={{
+												fontWeight: "800",
+												color: "#000000",
+												fontSize: 20,
+											}}>
+											{" "}
+											{user?.name ? user?.name : "User"}!
+										</Text>
+									</View>
+								)}
+							</View>
+						</>
+					)}
+				</>
+			)}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  header: {
-    height: "100%",
-    justifyContent: "center",
-    //alignItems: "center",
-    ...Platform.select({
-      web: {
-        marginTop: 5,
-      },
-    }),
-  },
-  appHeaderContainer: {
-    ...Platform.select({
-      web: {
-        flex: 1,
-      },
-    }),
-  },
-  appHeader: {
-    width: "100%",
-    height: 70,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 15,
-    ...Platform.select({
-      web: {
-        //gap: 100,
-      },
-    }),
-  },
-  hamburger: {
-    marginHorizontal: 5,
-  },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    zIndex: 50,
-  },
-  mobileSidebar: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "70%",
-    height: "100%",
-    backgroundColor: "#fff",
-    zIndex: 51,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-  },
-  authButtonsWeb: {
-    flexDirection: "row",
-    alignItems: "center",
-    alignSelf: "flex-end",
-    marginRight: 20,
-  },
-  authButtonsApp: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 15,
-    marginRight: "5%",
-  },
-  authButton: {
-    height: 50,
-    ...Platform.select({
-      web: {
-        padding: 10,
-        justifyContent: "center",
-      },
-    }),
-  },
-  authText: {
-    fontWeight: "800",
-    color: "#000000",
-    fontSize: 16,
-    ...Platform.select({
-      web: {
-        fontSize: 18,
-        color: "#FFFFFF",
-      },
-    }),
-  },
-  userInfo: {
-    marginTop: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10,
-  },
-  userInfoWeb: {
-    width: "100%",
-    justifyContent: "space-around",
-  },
-  userInfoApp: {},
-  userIcon: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-  },
-  username: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-  },
-  usernameApp: {
-    flexDirection: "row",
-    marginLeft: 20,
-  },
-  logoutButton: {
-    padding: 8,
-    backgroundColor: "#FF7072",
-    borderRadius: 5,
-  },
-  logoutText: {
-    color: "#FFFFFF",
-    fontSize: 16,
-  },
-  logo: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  dropdownImage: {
-    width: 50,
-    height: 50,
-  },
-  dropdownMain: {
-    position: "absolute",
-    backgroundColor: "#fff",
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    elevation: 5,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    zIndex: 100,
-  },
-  dropdownLoggedOut: {
-    top: 30,
-    right: 0,
-  },
-  dropdownLoggedIn: {
-    top: 40,
-    right: 0,
-  },
-  dropdownWeb: {
-    top: 40,
-    right: -70,
-  },
-  dropdownItem: {
-    padding: 10,
-  },
-  dropdownText: {
-    fontSize: 16,
-  },
-  welcomeContainer: {},
-  welcomeText: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  subText: {
-    fontSize: 14,
-    color: "#ddd",
-    marginTop: "1%",
-  },
-  searchContainer: {
-    flexDirection: "row",
-    alignSelf: "center",
-    borderRadius: 8,
-    paddingHorizontal: 15,
-    height: "80%",
-    width: "30%",
-    marginHorizontal: "10%",
-    borderWidth: 1,
-    borderColor: "rgba(255, 255, 255, 0.66)",
-    alignItems: "center",
-    backgroundColor: "rgba(255, 255, 255, 0.1)",
-  },
-  searchIcon: {
-    width: 16,
-    height: 16,
-    marginRight: 8,
-  },
-  searchInput: {
-    flex: 1,
-    color: "#fff",
-    fontSize: 16,
-    borderWidth: 0,
-    backgroundColor: "transparent",
-    paddingVertical: 0,
-    outlineStyle: "none",
-  },
-  iconsContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  notificationIcon: {
-    marginRight: 20,
-  },
-  profileWrapper: {
-    height: "60%",
-    width: "10%",
-    borderColor: "#fff",
-    alignSelf: "center",
-  },
-  profileContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 5,
-    borderRadius: 8,
-  },
-  profileIcon: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-  },
-
+	header: {
+		height: "100%",
+		justifyContent: "center",
+		//alignItems: "center",
+		...Platform.select({
+			web: {
+				marginTop: 5,
+			},
+		}),
+	},
+	appHeaderContainer: {
+		...Platform.select({
+			web: {
+				flex: 1,
+			},
+		}),
+	},
+	appHeader: {
+		width: "100%",
+		height: 70,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		paddingHorizontal: 15,
+		...Platform.select({
+			web: {
+				//gap: 100,
+			},
+		}),
+	},
+	hamburger: {
+		marginHorizontal: 5,
+	},
+	overlay: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundColor: "rgba(0, 0, 0, 0.5)",
+		zIndex: 50,
+	},
+	mobileSidebar: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: "70%",
+		height: "100%",
+		backgroundColor: "#fff",
+		zIndex: 51,
+	},
+	modalContainer: {
+		flex: 1,
+		justifyContent: "flex-start",
+		alignItems: "flex-start",
+	},
+	authButtonsWeb: {
+		flexDirection: "row",
+		alignItems: "center",
+		alignSelf: "flex-end",
+		marginRight: 20,
+	},
+	authButtonsApp: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 15,
+		marginRight: "5%",
+	},
+	authButton: {
+		height: 50,
+		...Platform.select({
+			web: {
+				padding: 10,
+				justifyContent: "center",
+			},
+		}),
+	},
+	authText: {
+		fontWeight: "800",
+		color: "#000000",
+		fontSize: 16,
+		...Platform.select({
+			web: {
+				fontSize: 18,
+				color: "#FFFFFF",
+			},
+		}),
+	},
+	userInfo: {
+		marginTop: 10,
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 10,
+	},
+	userInfoWeb: {
+		width: "100%",
+		justifyContent: "space-around",
+	},
+	userInfoApp: {},
+	userIcon: {
+		width: 40,
+		height: 40,
+		borderRadius: 20,
+	},
+	username: {
+		fontSize: 18,
+		fontWeight: "bold",
+		color: "#FFFFFF",
+	},
+	usernameApp: {
+		flexDirection: "row",
+		marginLeft: 20,
+	},
+	logoutButton: {
+		padding: 8,
+		backgroundColor: "#FF7072",
+		borderRadius: 5,
+	},
+	logoutText: {
+		color: "#FFFFFF",
+		fontSize: 16,
+	},
+	logo: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	dropdownImage: {
+		width: 50,
+		height: 50,
+	},
+	dropdownMain: {
+		position: "absolute",
+		backgroundColor: "#fff",
+		borderWidth: 1,
+		borderColor: "#ccc",
+		borderRadius: 5,
+		elevation: 5,
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		zIndex: 100,
+	},
+	dropdownLoggedOut: {
+		top: 30,
+		right: 0,
+	},
+	dropdownLoggedIn: {
+		top: 40,
+		right: 0,
+	},
+	dropdownWeb: {
+		top: 40,
+		right: -70,
+	},
+	dropdownItem: {
+		padding: 10,
+	},
+	dropdownText: {
+		fontSize: 16,
+	},
+	welcomeContainer: {},
+	welcomeText: {
+		fontSize: 28,
+		fontWeight: "bold",
+		color: "#fff",
+	},
+	subText: {
+		fontSize: 14,
+		color: "#ddd",
+		marginTop: "1%",
+	},
+	searchContainer: {
+		flexDirection: "row",
+		alignSelf: "center",
+		borderRadius: 8,
+		paddingHorizontal: 15,
+		height: "80%",
+		width: "30%",
+		marginHorizontal: "10%",
+		borderWidth: 1,
+		borderColor: "rgba(255, 255, 255, 0.66)",
+		alignItems: "center",
+		backgroundColor: "rgba(255, 255, 255, 0.1)",
+	},
+	searchIcon: {
+		width: 16,
+		height: 16,
+		marginRight: 8,
+	},
+	searchInput: {
+		flex: 1,
+		color: "#fff",
+		fontSize: 16,
+		borderWidth: 0,
+		backgroundColor: "transparent",
+		paddingVertical: 0,
+		outlineStyle: "none",
+	},
+	iconsContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	notificationIcon: {
+		marginRight: 20,
+	},
+	profileWrapper: {
+		height: "60%",
+		width: "10%",
+		borderColor: "#fff",
+		alignSelf: "center",
+	},
+	profileContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		padding: 5,
+		borderRadius: 8,
+	},
+	profileIcon: {
+		width: 40,
+		height: 40,
+		borderRadius: 20,
+	},
 });
 
 export default Header;
